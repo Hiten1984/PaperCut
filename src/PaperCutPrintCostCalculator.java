1e0c46ede050acb754cc8da2e36185cfef8b036f
Hiten1984 <hiten.bahri@gmail.com>
2015-12-23 02:51:53 +1100

    Added a new CSV File in root folder. Added new method to calculate the cost of print.

--- src/PaperCutPrintCostCalculator.java
+++ src/PaperCutPrintCostCalculator.java
@@ -5,79 +5,78 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 /**
  *  Simple Java that takes a list of A4 print jobs and calculates the cost of each job, 
- *   given the total number of pages, number of colour pages and whether printing is double sided.  
+ *   given the total number of pages, number of color pages and whether printing is double sided.  
  * 
  * */
 
 public class PaperCutPrintCostCalculator {
 	static int blackPage = 0;
 	static double costTotal = 0;
-	
+
 	public static void main(String... args) {
 		// Path of the file is used in the local directory.
-		List<PaperPrint> printCSV = readPrintJobsFromCSV("C:\\Users\\Ganu\\Downloads\\printjobs.csv");				
+		//List<PaperPrint> printCSV = readPrintJobsFromCSV("C:\\Users\\Ganu\\Downloads\\printjobs.csv");	
+		List<PaperPrint> printCSV = readPrintJobsFromCSV("printjobs.csv");
 		calculateJob(printCSV);	
 	}
 
-	private static void calculateJob(List<PaperPrint> pList) {
+	private static void calculateJob(List<PaperPrint> pList) {		
 		String printSide = null;
 		int tPage = 0;
 		int cPage = 0;
-		double bCost = 0;
-		double cCost = 0;
-		
+
 		for (PaperPrint paper : pList) {			
 			printSide = paper.getPrintSide();		
 			tPage = paper.getTotalPage();
 			cPage = paper.getColorPage();			
-			blackPage = tPage - cPage;	
-		
-			if("true".equalsIgnoreCase(printSide)) {
-				System.out.println("");
-				System.out.println("--- Cost Estimate ---");				
-				System.out.printf("Total number of pages - "+ tPage);
-				System.out.printf("%n");
-				System.out.printf("Total color pages - "+ cPage);
-				System.out.printf("%n");
-				System.out.printf("Total black pages - "+ blackPage);
-				cCost = cPage * 0.20;
-				System.out.printf("%n");
-				System.out.printf("Total color pages cost- $  "+ cCost);
-				bCost = blackPage * 0.10;
-				System.out.printf("%n");
-				System.out.printf("Total black pages cost- $  "+ bCost);			
-				costTotal = cCost + bCost;
-				System.out.printf("%n");
-				System.out.printf("Total Cost: single sided - $%6.2f%n", costTotal);
-			} else {
-				System.out.println("");
-				System.out.println("--- Cost Estimate ---");
-				System.out.printf("Total number of pages - "+ tPage);
-				System.out.printf("%n");
-				System.out.printf("Total color pages - "+ cPage);
-				System.out.printf("%n");
-				System.out.printf("Total black pages - "+ blackPage);
-				cCost = cPage * 0.25;
-				System.out.printf("%n");
-				System.out.printf("Total color pages cost- $  "+ cCost);
-				bCost = blackPage * 0.15;
-				System.out.printf("%n");
-				System.out.printf("Total black pages cost- $  "+ bCost);			
-				costTotal = cCost + bCost;
-				System.out.printf("%n");
-				System.out.printf("Total Cost: single sided - $%6.2f%n", costTotal);
-			}
+			blackPage = tPage - cPage;
+			countJobs(printSide,tPage, cPage, blackPage);
 		}
 	}
-	
 
+	public static void countJobs(String printSide, int tPage, int cPage, int blackPage) {
+		double bCost = 0;
+		double cCost = 0;
+		if(printSide.contains("true")) {
+			System.out.println("");
+			System.out.println("--- Cost Estimate ---");			
+			System.out.printf("Total number of pages - "+ tPage);
+			System.out.printf("%n");
+			System.out.printf("Total color pages - "+ cPage);
+			System.out.printf("%n");
+			System.out.printf("Total black and white pages - "+ blackPage);
+			cCost = cPage * 0.20;
+			System.out.printf("%n");
+			System.out.printf("Total color pages cost- $  "+ cCost);
+			bCost = blackPage * 0.10;
+			System.out.printf("%n");
+			System.out.printf("Total black pages cost- $  "+ bCost);			
+			costTotal = cCost + bCost;
+			System.out.printf("%n");
+			System.out.printf("Total Cost: single sided - $%6.2f%n", costTotal);
+		} else {
+			System.out.println("");
+			System.out.println("--- Cost Estimate ---");
+			System.out.printf("Total number of pages - "+ tPage);
+			System.out.printf("%n");
+			System.out.printf("Total color pages - "+ cPage);
+			System.out.printf("%n");
+			System.out.printf("Total black and white pages - "+ blackPage);
+			cCost = cPage * 0.25;
+			System.out.printf("%n");
+			System.out.printf("Total color pages cost- $  "+ cCost);
+			bCost = blackPage * 0.15;
+			System.out.printf("%n");
+			System.out.printf("Total black pages cost- $  "+ bCost);			
+			costTotal = cCost + bCost;
+			System.out.printf("%n");
+			System.out.printf("Total Cost: single sided - $%6.2f%n", costTotal);
+		}
+	}
 
 	private static List<PaperPrint> readPrintJobsFromCSV(String fileName) {
 		List<PaperPrint> print = new ArrayList<>();
@@ -86,14 +85,13 @@ public class PaperCutPrintCostCalculator {
 		// using try with resource, Java 7 feature to close resources
 		try (BufferedReader br = Files.newBufferedReader(pathToFile,
 				StandardCharsets.US_ASCII)) {
-			
+
 			String line = br.readLine();
-			
+
 			while (line != null) {
 				String[] attributes = line.split("\n");				
-				PaperPrint paperPrint = createPrintJob(attributes);			 
-				
-				print.add(paperPrint);
+				PaperPrint paperPrint = createPrintJob(attributes);
+				print.add(paperPrint);				
 				line = br.readLine();
 			}
 		} catch (IOException ioe) {
@@ -106,15 +104,12 @@ public class PaperCutPrintCostCalculator {
 	private static PaperPrint createPrintJob(String[] printData) {
 		int tPage = 0;
 		int cPage = 0;		
-		String pSide = null;
-
+		String pSide = null;		
 		for(String s : printData) {
 			String[] s2 = s.split(",");
-			for(String results : s2) {		       
-				tPage = Integer.parseInt(s2[0]);
-				cPage = Integer.parseInt(s2[1]);		  		
-				pSide = s2[2];
-			}		  	    
+			tPage = Integer.parseInt(s2[0]);
+			cPage = Integer.parseInt(s2[1]);		  		
+			pSide = s2[2];				
 		}
 
 		// create and return Paper Print object from Print Data 
@@ -150,9 +145,8 @@ class PaperPrint {
 
 	@Override
 	public String toString() {
-		return "PrintJob [Total Page = " + totalPage + 
+		return "Total Page = " + totalPage + 
 				", color page = " + colorPage +
-				", Double Print Side(True/False) = " + printSide
-				+ "]";
+				", Double Print Side(True/False) = " + printSide;
 	}
 }
